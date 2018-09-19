a131cdc9f95d1a2b3c68374c3d72ca305e5548b3
MarcYin <marcollinbobo@gmail.com>
2018-09-19 15:17:22 +0100

    add toy test

---
+++ tests/test_import.py
@@ -0,0 +1,15 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+user_pass = 'user', 'pass'
+
+with open(myPath.replace('tests', '') + '/SIAC/data/.earthdata_auth', 'wb') as f:
+    for i in user_pass:
+        f.write((i+'\n').encode())
+
+from SIAC import SIAC_S2
+from SIAC import SIAC_L8
+
+
+with open(myPath.replace('tests', '') + '/SIAC/data/.earthdata_auth', 'wb') as f:
+    f.write(('').encode())
