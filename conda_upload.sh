6311aa69e68a7038e3841fd567dc689f46140aaf
MarcYin <marcollinbobo@gmail.com>
2018-09-20 17:08:59 +0100

    auto conda

---
+++ conda_upload.sh
@@ -0,0 +1,11 @@
+# Only need to change these two variables
+PKG_NAME=siac
+USER=f0xy
+
+OS=$TRAVIS_OS_NAME-64
+mkdir ~/conda-bld
+conda config --set anaconda_upload no
+export CONDA_BLD_PATH=~/conda-bld
+export VERSION=`date +%Y.%m.%d`
+conda build . -c conda-forge -c uvcdat
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
