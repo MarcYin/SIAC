cae917782cd3b0ca6ba60d7f7f16d36d1bde42d0
MarcYin <marcollinbobo@gmail.com>
2018-09-24 17:15:45 +0100

    should work now....

--- SIAC/SIAC_S2.py
+++ SIAC/SIAC_S2.py
@@ -14,7 +14,7 @@ from os.path import expanduser
 home = expanduser("~")
 file_path = os.path.dirname(os.path.realpath(__file__))
 
-def SIAC_S2(s2_t, send_back = False, mcd43 = home + '/MCD43', vrt_dir = home + '/MCD43_VRT'):
+def SIAC_S2(s2_t, send_back = False, mcd43 = home + '/MCD43/', vrt_dir = home + '/MCD43_VRT/'):
     if not os.path.exists(file_path + '/emus/'):
         os.mkdir(file_path + '/emus/')
     if len(glob(file_path + '/emus/' + 'isotropic_MSI_emulators_*_x?p_S2?.pkl')) < 12:
@@ -40,15 +40,15 @@ def SIAC_S2(s2_t, send_back = False, mcd43 = home + '/MCD43', vrt_dir = home + '
         return aero_atmos
 
 def do_correction(sun_ang_name, view_ang_names, toa_refs, cloud_name, \
-                  cloud_mask, metafile, mcd43 = home + '/MCD43', vrt_dir = home + '/MCD43_VRT'):
+                  cloud_mask, metafile, mcd43 = home + '/MCD43/', vrt_dir = home + '/MCD43_VRT/'):
 
-    if os.path.realpath(mcd43) in os.path.realpath(home + '/MCD43'):
-        if not os.path.exists(home + '/MCD43'):
-            os.mkdir(home + '/MCD43')
+    if os.path.realpath(mcd43) in os.path.realpath(home + '/MCD43/'):
+        if not os.path.exists(home + '/MCD43/'):
+            os.mkdir(home + '/MCD43/')
 
-    if os.path.realpath(vrt_dir) in os.path.realpath(home + '/MCD43_VRT'):
-        if not os.path.exists(home + '/MCD43_VRT'):
-            os.mkdir(home + '/MCD43_VRT')
+    if os.path.realpath(vrt_dir) in os.path.realpath(home + '/MCD43_VRT/'):
+        if not os.path.exists(home + '/MCD43_VRT/'):
+            os.mkdir(home + '/MCD43_VRT/')
 
     base = os.path.dirname(toa_refs[0])
     base = toa_refs[0].replace('B01.jp2', '')
