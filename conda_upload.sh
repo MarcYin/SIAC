5547b84cffabbfbb09cac9bfecdd2aced1c29046
MarcYin <marcollinbobo@gmail.com>
2018-11-08 16:41:21 +0000

    better build

--- conda_upload.sh
+++ conda_upload.sh
@@ -5,10 +5,9 @@ OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld 
 conda config --set anaconda_upload no  
 export CONDA_BLD_PATH=~/conda-bld 
-export VERSION=`date +%Y.%m.%d`  
 conda build . -c conda-forge
 ls -lah $CONDA_BLD_PATH/$OS 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $(ls $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION*.tar.bz2) --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $(ls $CONDA_BLD_PATH/$OS/$PKG_NAME-$SIAC_VERSION*.tar.bz2) --force
 
 # Only need to change these two variables
 #PKG_NAME=siac
