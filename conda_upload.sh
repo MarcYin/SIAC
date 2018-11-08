dfc722c2b88e459b2530856719ead5d052765dca
MarcYin <marcollinbobo@gmail.com>
2018-11-08 12:06:11 +0000

    purify travis.yml

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,13 +1,24 @@
-# Only need to change these two variables
-PKG_NAME=siac
-USER=f0xy
-
-OS=$TRAVIS_OS_NAME-64
-mkdir ~/conda-bld
-conda config --set anaconda_upload no
-export CONDA_BLD_PATH=~/conda-bld
-export VERSION=`date +%Y.%m.%d`
+conda install conda-build  
+conda install anaconda-client 
+PKG_NAME=siac && USER=f0xy  
+OS=$TRAVIS_OS_NAME-64  
+mkdir ~/conda-bld 
+conda config --set anaconda_upload no  
+export CONDA_BLD_PATH=~/conda-bld 
+export VERSION=`date +%Y.%m.%d`  
 conda build . -c conda-forge
-ls $CONDA_BLD_PATH
-ls $CONDA_BLD_PATH/$OS
+ls -lah $CONDA_BLD_PATH/$OS 
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $(ls $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION*.tar.bz2) --force
+
+# Only need to change these two variables
+#PKG_NAME=siac
+#USER=f0xy
+#OS=$TRAVIS_OS_NAME-64
+#mkdir ~/conda-bld
+#conda config --set anaconda_upload no
+#export CONDA_BLD_PATH=~/conda-bld
+#export VERSION=`date +%Y.%m.%d`
+#conda build . -c conda-forge
+#ls $CONDA_BLD_PATH
+#ls $CONDA_BLD_PATH/$OS
+#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $(ls $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION*.tar.bz2) --force
