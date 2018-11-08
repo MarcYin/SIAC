6614979ef6b9707cd2cc97f9596ee1563ec4beb0
MarcYin <marcollinbobo@gmail.com>
2018-11-08 18:31:36 +0000

    fix version for conda and pypi

--- conda_upload.sh
+++ conda_upload.sh
@@ -5,9 +5,10 @@ OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld 
 conda config --set anaconda_upload no  
 export CONDA_BLD_PATH=~/conda-bld 
+export VERSION=$SIAC_VERSION
 conda build . -c conda-forge
 ls -lah $CONDA_BLD_PATH/$OS 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $(ls $CONDA_BLD_PATH/$OS/$PKG_NAME-$SIAC_VERSION*.tar.bz2) --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $(ls $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION*.tar.bz2) --force
 
 # Only need to change these two variables
 #PKG_NAME=siac
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'rb') as f:
     readme = f.read().decode()
 
 setup(name                          = 'SIAC',
-      version                       = verison,
+      version                       = version,
       description                   = 'A sensor invariant Atmospheric Correction (SIAC)',
       long_description              = readme,
       long_description_content_type ='text/markdown',
