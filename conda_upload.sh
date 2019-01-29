aef292ff6f618e7336641d0d1dada9edad73c722
MarcYin <marcollinbobo@gmail.com>
2019-01-29 01:08:55 +0000

    put together

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,7 +1,6 @@
 conda install conda-build  
 conda install anaconda-client 
 PKG_NAME=siac && USER=f0xy  
-OS=$TRAVIS_OS_NAME-64  
 mkdir ~/conda-bld 
 conda config --set anaconda_upload no  
 export CONDA_BLD_PATH=~/conda-bld 
@@ -9,8 +8,6 @@ export VERSION=$SIAC_VERSION
 conda build .
 export CONDA_PACKAGE=`conda build --output . | grep bz2`
 echo $CONDA_PACKAGE
-ls -lah $CONDA_BLD_PATH/$OS 
-ls -lah $CONDA_BLD_PATH/siac*
 ls -lah $CONDA_BLD_PATH/noarch
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_PACKAGE --force
 
